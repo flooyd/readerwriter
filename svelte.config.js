0fe4e84f9742ef75f487307f99a3469565b990e9
Floyd Jones <floydtjones@gmail.com>
2022-12-13 01:03:04 -0600

    ...

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,7 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		vite: {
-			optimizeDeps: {
-				exclude: ['mongodb']
-			}
-		}
+		adapter: adapter()
 	}
 };
 
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	optimizeDeps: {
+		exclude: ['mongodb']
 	}
 };
 
