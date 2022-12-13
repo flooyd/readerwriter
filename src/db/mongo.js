403486510c8a6f4019d97a66e4bc6b1faa63ae58
Floyd Jones <floydtjones@gmail.com>
2022-12-13 01:13:30 -0600

    ...

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 import { MONGO_URL } from '$env/static/private';
 
-const client = new MongoClient(MONGO_URL, { useUnifiedTopology: true });
+const client = new MongoClient(MONGO_URL, { useUnifiedTopology: false });
 
 export function start_mongo() {
 	console.log('MONGO_URL', MONGO_URL);
